55d157acace2ee091faca1bb895184588489162a
Safayatcc20 <mdsafayatcc20@gmail.com>
2024-11-28 23:59:11 +0600

    Safayat

---
+++ Topic_and_link/Stars_and_Bars/Problem_Makes_Problem.cpp
@@ -0,0 +1,142 @@
+ 
+ /***** Bismillahir Rahmanir Rahim *****/
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template <typename T> using oset = tree<T , null_type , less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define sad '\n'
+#define all(b) b.begin(), b.end()
+#define ha cout << "YES" << '\n'
+#define na cout << "NO" << '\n'
+#define minusone cout << "-1" << '\n'
+#define int long long
+long long t = 1;
+constexpr bool multiple_test_cases = 1;
+long long gcd(long long x, long long y)
+{
+    if (y == 0)
+        return x;
+    return gcd(y, x % y);
+}
+long long lcm(long long x, long long y)
+{
+    return ((x * y) / gcd(x, y));
+}
+long long fact(long long n)
+{
+    if (n == 0)
+        return 1;
+    return n * fact(n - 1);
+}
+long long findXor(long long n){//reming: function calling findXor(sending value - 1);
+    long long rest = n % 4;
+    if(rest == 0) return n;
+    if(rest == 1) return 1;
+    if(rest == 2) return n+1;
+    if(rest == 3) return 0;
+}
+// Linear Diophantine Equation ax + by = c;
+// if c % gcd( a , b)  == 0 then solution exist otherwise not
+//solution by using  Extended Euclidean algorithm one solution
+int extgcd(int a, int b, int& x, int& y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int d = extgcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
+// Euler totient function for single call
+int phi(int n){
+  int ans = n;
+  for(int i = 2; i * i <= n ;i++){ 
+   if(n % i == 0){
+       ans /= i;
+       ans *= (i-1);
+       while(n % i== 0) n/= i;
+   }
+  }
+  if(n > 1) ans /= n , ans *= (n - 1);
+  return ans;
+}
+//Euler totient preprocessing(from 1 to n ) in O(nloglog(n))
+vector<int>phai(1e7+10);
+void init(){
+// here check the problem requirement
+     int maxn = 1e7+10;
+     for(int i = 1; i <= maxn; i++){
+       phai[i] = i;
+     }
+     for(int i = 2; i<= maxn; i++){
+       if(phai[i] == i){
+         for(int j = i; j <= maxn; j+=i){
+           phai[j]/=i; phai[j] *= (i-1);
+         }
+       }
+     }
+     // here is the prefix sum of phai function
+     // for(int i = 1; i <= maxn; i++){
+     //     phai[i] += phai[i-1];
+     // }
+}
+//recurrence nCk = (n-1)C(k) + (n-1)C(k-1)
+//symmetry rule nCk = nC(n-k)
+//nCr = summation of (0 to r) range ∑(n-1+j)C(j) here 0<=j <=r
+//sum over k ∑(nCk) = 2^n where 0<=k <= n
+// sum over n ∑(mCk)= (n+1)C(k+1) where k is fixed and 0<=m <= n
+// sum over n and k  ∑(n+k)C(k)= (n+m+1)C(m) where 0<= k<= m
+// sum of squars (nC0)^2 + (nC1)^2+...+(nCn)^2 = (2*n)C(n);
+// weighted sum 0*(nC0)+ 1*(nC1)+2*(nC2)...+n(nCn)^2 = (2^(n-1))*(n);
+// Fibonacci number (nC0) + (n-1)C1 + ...+(n-k)Ck+...+0Cn = F(n+1);
+
+//Keep mind to call prec in main 
+//carefull for constraint N
+const int N = 2e6 + 9, mod = 1e9 + 7;
+// Using Extended Euclidean Algorithm 
+int f[N], inv[N], finv[N];
+void prec() {
+  f[0] = 1;
+  for (int i = 1; i < N; i++) f[i] = 1LL * i * f[i - 1] % mod;
+  inv[1] = 1;
+  for (int i = 2; i < N; i++ ) {
+    inv[i] = (-(1LL * mod / i) * inv[mod % i] ) % mod;
+    inv[i] = (inv[i] + mod) % mod;
+  }
+  finv[0] = 1;
+  for (int i = 1; i < N; i++) finv[i] = 1LL * inv[i] * finv[i - 1] % mod;
+}
+int nCr(int n, int r) {
+  if (n < r || n < 0 || r < 0) return 0;
+  return 1LL * f[n] * finv[n - r] % mod * finv[r] % mod;
+}
+void solve(){
+    int n , k;
+    cin >> n >> k;
+    int ans = nCr(n+ k - 1, n )%mod;
+    cout << ans << sad;
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    // long long t;
+    prec();
+    cin >> t;
+    int i = 1;
+    while (t--)
+    {
+        cout<< "Case "<<i<<": ";
+        i++;
+        solve();
+    }
+}
